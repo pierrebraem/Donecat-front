f95be800122db44fdf6b359a0d28e82845523e2d
Pierre Braem <pierrebraem@orange.fr>
2025-06-02 11:18:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import InputText from "primevue/inputtext"
+import Button from "primevue/button"
+import { ref } from "vue";
+
+const text = ref("")
+</script>
+
+<template>
+    <h1 class="text-3xl font-bold underline">Hello World!</h1>
+    <div class="flex flex-col space-y-2">
+      <InputText v-model="text" class="w-30" />
+      <Button label="Verify" severity="info" class="w-16" />
+    </div>
+    <p>{{ text }}</p>
+</template>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './assets/main.css'
+
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
+app.mount('#app')
