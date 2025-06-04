814ef6a074494168081d8c367654a4e07bfc2da1
Pierre Braem <pierrebraem@orange.fr>
2025-06-04 14:15:56 +0200

    feat: récupère données de la bdd

--- src/views/Utilisateurs.vue
+++ src/views/Utilisateurs.vue
@@ -2,44 +2,19 @@
 import { Button, Card, ConfirmDialog } from 'primevue'
 import { useConfirm } from 'primevue/useconfirm'
 import AjoutUtilisateur from '@/components/modals/AjoutUtilisateur.vue'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const confirm = useConfirm()
 
 const visibleAjoutUtilisateur = ref(false)
+const chargement = ref(true)
+const utilisateurs = ref([])
 
-const utilisateurs = ref([
-    {
-        id: 1,
-        nom: "Braem",
-        prenom: "Pierre",
-        email: "pierre.braem@test.com",
-        pseudo: "MonsieurBraem",
-        motdepasse: "motdepasse",
-        status: "administrateur",
-        equipe_id: 1
-    },
-    {
-        id: 2,
-        nom: "Dupont",
-        prenom: "Martin",
-        email: "martin.dupont@test.com",
-        pseudo: "xXDarkDu59Xx",
-        motdepasse: "motdepasse",
-        status: "manager",
-        equipe_id: 2
-    },
-    {
-        id: 3,
-        nom: "Rain",
-        prenom: "Jean",
-        email: "jean.rain@test.com",
-        pseudo: "MonsieurRain",
-        motdepasse: "motdepasse",
-        status: "dev",
-        equipe_id: 1
-    }
-])
+async function getUtilisateurs(){
+    const response = await fetch("http://localhost:3000/utilisateurs")
+    const data = await response.json();
+    utilisateurs.value = data
+}
 
 function supprimerUtilisateur(nom, prenom){
     confirm.require({
@@ -55,38 +30,53 @@ function supprimerUtilisateur(nom, prenom){
         }
     })
 }
+
+onMounted(async () => {
+    await getUtilisateurs()
+    chargement.value = false
+})
 </script>
 
 <template>
-    <div class="pt-4 flex flex-col space-y-6">
-        <div class="flex justify-end pr-4">
-            <Button label="Créer un utilisateur" severity="info" @click="visibleAjoutUtilisateur = true" />
-        </div>
-        <div class="flex justify-center">
-            <Card class="w-11/12">
-                <template #content>
-                    <div class="flex flex-col space-y-2">
-                        <template v-for="utilisateur in utilisateurs">
-                            <Card style="background-color: aqua;" v-if="utilisateur.status != 'administrateur'">
-                                <template #content>
-                                    <div class="flex flex-col space-y-2">
-                                        <p class="font-bold">{{ utilisateur.nom.toUpperCase() }} {{ utilisateur.prenom }}</p>
-                                        <p>{{ utilisateur.email }}</p>
-                                        <p>{{ utilisateur.status }}</p>
-                                    </div>
-                                    <div class="flex justify-end gap-2">
-                                        <Button label="Modifier" severity="warn" />
-                                        <Button label="Supprimer" severity="danger" @click="supprimerUtilisateur(utilisateur.nom.toUpperCase(), utilisateur.prenom)" />
-                                    </div>
-                                </template>
-                            </Card>
+    <template v-if="chargement">
+        <p class="text-center">Chargement des données...</p>
+    </template>
+    <template v-else>
+        <div class="pt-4 flex flex-col space-y-6">
+            <div class="flex justify-end pr-4">
+                <Button label="Créer un utilisateur" severity="info" @click="visibleAjoutUtilisateur = true" />
+            </div>
+            <div class="flex justify-center">
+                <Card class="w-11/12">
+                    <template #content>
+                        <template v-if="utilisateurs.length == 0">
+                            <p class="text-center">Aucun utilisateur enregistré</p>
                         </template>
-                    </div>
-                </template>
-            </Card>
+                        <template v-else>
+                            <div class="flex flex-col space-y-2">
+                                <template v-for="utilisateur in utilisateurs">
+                                    <Card style="background-color: aqua;" v-if="utilisateur.status != 'Administrateur'">
+                                        <template #content>
+                                            <div class="flex flex-col space-y-2">
+                                                <p class="font-bold">{{ utilisateur.nom.toUpperCase() }} {{ utilisateur.prenom }}</p>
+                                                <p>{{ utilisateur.email }}</p>
+                                                <p>{{ utilisateur.status }}</p>
+                                            </div>
+                                            <div class="flex justify-end gap-2">
+                                                <Button label="Modifier" severity="warn" />
+                                                <Button label="Supprimer" severity="danger" @click="supprimerUtilisateur(utilisateur.nom.toUpperCase(), utilisateur.prenom)" />
+                                            </div>
+                                        </template>
+                                    </Card>
+                                </template>
+                            </div>
+                        </template>
+                    </template>
+                </Card>
+            </div>
         </div>
-    </div>
 
-    <AjoutUtilisateur v-model:visible="visibleAjoutUtilisateur" />
-    <ConfirmDialog />
+        <AjoutUtilisateur v-model:visible="visibleAjoutUtilisateur" />
+        <ConfirmDialog />
+    </template>
 </template>
