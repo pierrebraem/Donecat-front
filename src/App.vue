e7073f8a7da79868a52432b4daa4ef5ca47b1c8e
Pierre Braem <pierrebraem@orange.fr>
2025-06-04 16:25:47 +0200

    feat: navbar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,57 @@
+<script setup>
+import { Menubar } from "primevue"
+import { ref } from "vue"
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const items = ref([
+    {
+        label: 'Accueil',
+        command: () => {
+            router.push('/')
+        }
+    },
+    {
+        label: 'Administration',
+        command: () => {
+            router.push('/gestion')
+        }
+    },
+    {
+        label: 'Liste des tâches',
+        command: () => {
+            router.push('/taches')
+        }
+    },
+    {
+        label: 'Gestion des utilisateurs',
+        command: () => {
+            router.push('/utilisateurs')
+        }
+    },
+    {
+        label: 'Profil',
+        command: () => {
+            router.push('/profil')
+        }
+    },
+    {
+        label: 'Connexion',
+        command: () => {
+            router.push('/connexion')
+        }
+    }
+])
+</script>
+
 <template>
+    <Menubar :model="items">
+        <template #item="{ item, props }">
+            <a v-ripple :href="item.url" :target="item.target" v-bind="props.action">
+                <span>{{ item.label }}</span>
+            </a>
+        </template>
+    </Menubar>
     <router-view></router-view>
 </template>
