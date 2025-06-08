a3e0e2f5b6f8d12fc111474fa43a9f113b0a795e
Pierre Braem <pierrebraem@orange.fr>
2025-06-08 21:36:58 +0200

    Fix: rond chargement qui ne tournais pas

--- src/components/Chargement.vue
+++ src/components/Chargement.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex justify-center">
-    <span class="pi pi-spon pi-spinner" style="font-size: 3rem" />
+    <span class="pi pi-spin pi-spinner" style="font-size: 3rem" />
   </div>
 </template>
