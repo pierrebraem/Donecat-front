0e75f5b8cc8e192906a09186ec5b43d5f8fa9299
Pierre Braem <pierrebraem@orange.fr>
2026-02-05 15:32:12 +0100

    Configuration Cloud Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:5173',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:4173',
+    projectId: "8e9uf9",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
