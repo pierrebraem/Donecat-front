942f9e2c8cd986f37da68ccd395bcecb23370c32
Pierre Braem <pierrebraem@orange.fr>
2026-02-06 08:46:42 +0100

    Tentative réparation test 1

--- cypress/e2e/profil.cy.js
+++ cypress/e2e/profil.cy.js
@@ -160,10 +160,12 @@ describe('Tests de la modal "Modification du profil"', () => {
     cy.get("#modifier-utilisateur-valider").click();
 
     cy.get(".p-dialog-header").should("exist");
-    cy.get("#modifier-utilisateur-erreurs").should(
-      "contain",
-      'L\'adresse mail "ancelina.beausoleil@test.com" existe déjà. Veuillez en prendre un autre.',
-    );
+    cy.get("#modifier-utilisateur-erreurs", { timeout: 10000 })
+      .should("be.visible")
+      .and(
+        "contain",
+        'L\'adresse mail "ancelina.beausoleil@test.com" existe déjà. Veuillez en prendre un autre.',
+      );
   });
 
   it('Supprime le contenu de champ "Pseudo" et clique sur le bouton "Modifier"', () => {
