638e011e34fc9acd9eb588955d4bdd8522adbf81
Pierre Braem <pierrebraem@orange.fr>
2026-02-06 09:38:10 +0100

    Fix: Correction tests affichage

--- cypress/e2e/profil.cy.js
+++ cypress/e2e/profil.cy.js
@@ -24,19 +24,19 @@ describe("Vérifie que les données se charge correctement", () => {
     cy.intercept("GET", "/utilisateurs/1", { fixture: "profil/admin.json" });
     cy.visit("http://localhost:5173/profil");
 
-    cy.get('div[id="profil-nom"]').should("contain", "Nom :LaPierre");
-    cy.get('div[id="profil-prenom"]').should("contain", "Prénom :Françoise");
+    cy.get('div[id="profil-nom"]').should("contain", "Nom : LaPierre");
+    cy.get('div[id="profil-prenom"]').should("contain", "Prénom : Françoise");
     cy.get('div[id="profil-email"]').should(
       "contain",
-      "Email :francoise.lapierre@test.com",
+      "Email : francoise.lapierre@test.com",
     );
     cy.get('div[id="profil-pseudo"]').should(
       "contain",
-      "Pseudo :FrançoiseDu80",
+      "Pseudo : FrançoiseDu80",
     );
     cy.get('div[id="profil-status"]').should(
       "contain",
-      "Status :Administrateur",
+      "Status : Administrateur",
     );
   });
 });
@@ -204,16 +204,16 @@ describe('Tests de la modal "Modification du profil"', () => {
 
     cy.get("#modifier-utilisateur-valider").click();
 
-    cy.get('div[id="profil-nom"]').should("contain", "Nom :Doyon");
-    cy.get('div[id="profil-prenom"]').should("contain", "Prénom :Michèle");
+    cy.get('div[id="profil-nom"]').should("contain", "Nom : Doyon");
+    cy.get('div[id="profil-prenom"]').should("contain", "Prénom : Michèle");
     cy.get('div[id="profil-email"]').should(
       "contain",
-      "Email :francoise.lapierre@test.com",
+      "Email : francoise.lapierre@test.com",
     );
-    cy.get('div[id="profil-pseudo"]').should("contain", "Pseudo :MicheleDu59");
+    cy.get('div[id="profil-pseudo"]').should("contain", "Pseudo : MicheleDu59");
     cy.get('div[id="profil-status"]').should(
       "contain",
-      "Status :Administrateur",
+      "Status : Administrateur",
     );
   });
 
@@ -232,16 +232,19 @@ describe('Tests de la modal "Modification du profil"', () => {
 
     cy.get("#modifier-utilisateur-valider").click();
 
-    cy.get('div[id="profil-nom"]').should("contain", "Nom :Patry");
-    cy.get('div[id="profil-prenom"]').should("contain", "Prénom :Marmion");
+    cy.get('div[id="profil-nom"]').should("contain", "Nom : Patry");
+    cy.get('div[id="profil-prenom"]').should("contain", "Prénom : Marmion");
     cy.get('div[id="profil-email"]').should(
       "contain",
-      "Email :marmion.patry@test.com",
+      "Email : marmion.patry@test.com",
     );
-    cy.get('div[id="profil-pseudo"]').should("contain", "Pseudo :PatryMarmion");
+    cy.get('div[id="profil-pseudo"]').should(
+      "contain",
+      "Pseudo : PatryMarmion",
+    );
     cy.get('div[id="profil-status"]').should(
       "contain",
-      "Status :Administrateur",
+      "Status : Administrateur",
     );
   });
 });
